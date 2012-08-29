d920bc0a2544e37410e2f0c8b528f490a1b5aaa7
Emily Ellison <emilyellison986@gmail.com>
2012-08-29 08:56:17 -0500

    Set up model associations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,6 @@
 class Note < ActiveRecord::Base
   attr_accessible :category_id, :description, :retro_id
+  
+  belongs_to :category
+  belongs_to :retro
 end
--- app/models/retro.rb
+++ app/models/retro.rb
@@ -1,3 +1,6 @@
 class Retro < ActiveRecord::Base
   attr_accessible :sprint, :team_id
+  
+  belongs_to :team
+  has_many :notes
 end
