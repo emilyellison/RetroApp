9369763f849b3224f4ea2a2987087d80affac188
Emily Ellison <emilyellison986@gmail.com>
2012-08-29 08:51:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RetroApp::Application.config.secret_token = '2f41509a1914e5f7d7949a45a115ba844c201cb76db25372e3e08136d1ff2b1ea7bbf8d0616145c91379ead5d61fd6b4e448a27a9d11121f2fdffccd763175c5'
